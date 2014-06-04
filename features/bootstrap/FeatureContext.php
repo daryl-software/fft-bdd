098e9f561c25b4594ce8d3972f02dbe2aa2e0d33
Mathieu Rochette <mathieu@texthtml.net>
2014-06-05 01:11:54 +0200

    step-2

--- features/bootstrap/FeatureContext.php
+++ features/bootstrap/FeatureContext.php
@@ -1,13 +1,15 @@
 <?php
 
+use Behat\Behat\Tester\Exception\PendingException;
 use Behat\Behat\Context\SnippetAcceptingContext;
 use Behat\Gherkin\Node\PyStringNode;
 use Behat\Gherkin\Node\TableNode;
+use Behat\MinkExtension\Context\MinkContext;
 
 /**
  * Behat context class.
  */
-class FeatureContext implements SnippetAcceptingContext
+class FeatureContext extends MinkContext implements SnippetAcceptingContext
 {
     /**
      * Initializes context.
@@ -17,5 +19,37 @@ class FeatureContext implements SnippetAcceptingContext
      */
     public function __construct()
     {
+    }
+
+    /**
+     * @Given Bob is on the ticket creation page
+     */
+    public function bobIsOnTheTicketCreationPage()
+    {
+        throw new PendingException();
+    }
+
+    /**
+     * @Given he fill :arg1 with :arg2
+     */
+    public function heFillWith($arg1, $arg2)
+    {
+        throw new PendingException();
+    }
+
+    /**
+     * @When he press :arg1
+     */
+    public function hePress($arg1)
+    {
+        throw new PendingException();
+    }
+
+    /**
+     * @Then he should see :arg1
+     */
+    public function heShouldSee($arg1)
+    {
+        throw new PendingException();
     }
 }
