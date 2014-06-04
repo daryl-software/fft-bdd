a84a8fa633dd25b01e276dadc56d74cafab0426d
Mathieu Rochette <mathieu@texthtml.net>
2014-06-05 01:45:08 +0200

    step-5

---
+++ public/new_ticket.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<h1>Ticket Creation</h1>
+<form method="post">
+    <label>
+        name
+        <input name="name"/>
+    </label><br/>
+    <label>
+        description
+        <textarea name="description"></textarea>
+    </label>
+    <input type="submit" value="Create ticket"/>
+</form>
